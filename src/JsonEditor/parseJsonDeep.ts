ca5e82d5323f71c5222e445fc9295ccc99dba85c
peishuangheng <peishuangheng@bytedance.com>
2024-11-25 12:26:12 +0800

    feat: 添加类型注释

--- src/JsonEditor/parseJsonDeep.ts
+++ src/JsonEditor/parseJsonDeep.ts
@@ -1,15 +1,17 @@
-export function parseJsonDeep(target: any) {
-  let parsedTarget: any;
+import { keys } from "lodash";
+
+export function parseJsonDeep(target: unknown) {
+  let parsedTarget: Record<string, unknown> | string;
 
   try {
-    parsedTarget = JSON.parse(target);
+    parsedTarget = JSON.parse(target as string);
   } catch (e) {
     console.error("Failed to parse JSON:", e);
-    parsedTarget = target;
+    parsedTarget = target as string;
   }
 
   if (typeof parsedTarget === "object" && parsedTarget !== null) {
-    Reflect.ownKeys(parsedTarget).forEach((key) => {
+    keys(parsedTarget).forEach((key) => {
       parsedTarget[key] = parseJsonDeep(parsedTarget[key]);
     });
   } else if (Array.isArray(parsedTarget)) {
